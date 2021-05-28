0b98262a8f6ffd518d32f2dca1cf2dacc46332f8
almersesunan <almer.sesunan@gmail.com>
2021-05-29 00:26:43 +0700

    cashflow page temp

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -120,7 +120,7 @@
         }
     },
     series: [{
-        name: 'Bulan',
+        name: 'Jumlah',
         data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
 
     }]
@@ -177,10 +177,11 @@
         enabled: false
     },
     series: [{
-        name: 'Nama Barang',
+        name: 'Jumlah',
         data: [107, 31, 635, 203, 2]
     }]
+    
 });
 </script>
 
-@stop
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('summary');
 });
 
+Route::get('/cashflow', function () {
+    return view('cashflow');
+});
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
