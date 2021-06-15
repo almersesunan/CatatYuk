56ba00c34f64732847cd7cd4631b56da3ada6110
almersesunan <almer.sesunan@gmail.com>
2021-06-15 19:10:55 +0700

    feedback button

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,18 +11,13 @@
             crossorigin="anonymous"
         />
         <!-- Bootstrap CSS -->
-        <link
+    <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
-<<<<<<< HEAD
             crossorigin="anonymous"
         />
         <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script> -->
-=======
-            crossorigin="anonymous"/>
-        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
->>>>>>> 4b28545c0f109f0385efa80e3f1fa031e075c27b
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="/css/app.css">
     <link rel="stylesheet" type="text/css" href="/css/main.css">
@@ -118,9 +113,7 @@
       
     @yield('container')
     <div class="feedback-area" style="padding: 10px; text-align: right;">
-        <a href="#" class="link-feedback">
-            Send us your feedback.
-        </a>
+        <button type="button" class="btn btn-primary btn-sm" href="#">Send us your feedback.</button>
     </div>
 
     @yield('footer')
