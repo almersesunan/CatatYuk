286810ee4d412625e2d4fa898b13e6a57de609b8
almersesunan <almer.sesunan@gmail.com>
2021-06-24 00:29:50 +0700

    redo footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -198,167 +198,167 @@
           <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 @yield('container')
-        
-                <div class="feedback-area" style="padding: 10px; text-align: right;">
-                    <a href="feedback"><button type="button" class="btn btn-link btn-sm" >Send us your feedback!</button></a>
-                </div>
-            
-                 <!-- Footer -->
-            
-                 <footer class="text-center text-lg-start bg-light text-muted">
-                        <!-- Section: Social media -->
-                        <section
-                            class="
-                                d-flex
-                                justify-content-center justify-content-lg-between
-                                p-4
-                                border-bottom
-                            "
-                        >
-                            <!-- Left -->
-                            <div class="me-5 d-none d-lg-block">
-                                <span>Get connected with us on social networks:</span>
-                            </div>
-                            <!-- Left -->
-                        
-                            <!-- Right -->
-                            <div>
-                                <a href="" class="me-4 text-reset">
-                                    <i class="fab fa-facebook-f"></i>
-                                </a>
-                                <a href="" class="me-4 text-reset">
-                                    <i class="fab fa-twitter"></i>
-                                </a>
-                                <a href="" class="me-4 text-reset">
-                                    <i class="fab fa-google"></i>
-                                </a>
-                                <a href="" class="me-4 text-reset">
-                                    <i class="fab fa-instagram"></i>
-                                </a>
-                                <a href="" class="me-4 text-reset">
-                                    <i class="fab fa-linkedin"></i>
-                                </a>
-                            </div>
-                            <!-- Right -->
-                        </section>
-                        <!-- Section: Social media -->
-                    
-                        <!-- Section: Links  -->
-                        <section class="">
-                            <div class="container text-center text-md-start mt-5">
-                                <!-- Grid row -->
-                                <div class="row mt-3">
-                                    <!-- Grid column -->
-                                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                                        <!-- Content -->
-                                        <h6 class="text-uppercase fw-bold mb-4">
-                                            <i class="fas fa-gem me-3"></i>CatatYuk!
-                                        </h6>
-                                        <p>
-                                            Kami ingin membantu menjadikan aktifitas
-                                            mencatat semua transaksi, membuat laporan
-                                            keuangan, pencatatan stok barang, dan mencatat &
-                                            menagih hutang piutang usaha kamu menjadi
-                                            semakin mudah.
-                                        </p>
-                                    </div>
-                                    <!-- Grid column -->
-                                
-                                    <!-- Grid column -->
-                                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                                        <!-- Links -->
-                                        <h6 class="text-uppercase fw-bold mb-4">
-                                            Products
-                                        </h6>
-                                        <p>
-                                            <a href="#!" class="text-reset"
-                                                >Syarat & Ketentuan</a
-                                            >
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-reset"
-                                                >Kebijakan Privasi</a
-                                            >
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-reset">FAQ</a>
-                                        </p>
-                                    </div>
-                                    <!-- Grid column -->
-                                
-                                    <!-- Grid column -->
-                                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                                        <!-- Links -->
-                                        <h6 class="text-uppercase fw-bold mb-4">
-                                            Perusahaan
-                                        </h6>
-                                        <p>
-                                            <a href="#!" class="text-reset">Tentang Kami</a>
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-reset">Karir</a>
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-reset"
-                                                >Blog & Cerita</a
-                                            >
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-reset">Kontak Kami</a>
-                                        </p>
-                                    </div>
-                                    <!-- Grid column -->
-                                
-                                    <!-- Grid column -->
-                                    <div
-                                        class="
-                                            col-md-4 col-lg-3 col-xl-3
-                                            mx-auto
-                                            mb-md-0 mb-4
-                                        "
-                                    >
-                                        <!-- Links -->
-                                        <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-                                        <p>
-                                            <i class="fas fa-home me-3"></i> Jakarta,
-                                            Jakarta Selatan 12530, Indonesia
-                                        </p>
-                                        <p>
-                                            <i class="fas fa-envelope me-3"></i>
-                                            CatatYuk@gmail.com
-                                        </p>
-                                        <p>
-                                            <i class="fas fa-phone me-3"></i> + 62 878 8652
-                                            8278
-                                        </p>
-                                        <p>
-                                            <i class="fas fa-print me-3"></i> + 01 234 567
-                                            89
-                                        </p>
-                                    </div>
-                                    <!-- Grid column -->
-                                </div>
-                                <!-- Grid row -->
-                            </div>
-                        </section>
-                        <!-- Section: Links  -->
-                    
-                        <!-- Copyright -->
-                        <div
-                            class="text-center p-4"
-                            style="background-color: rgba(0, 0, 0, 0.05)"
-                        >
-                            © 2021 Copyright:
-                            <a class="text-reset fw-bold" href="https://CatatYuk.com/"
-                                >CatatYuk.com</a
-                            >
-                        </div>
-                        <!-- Copyright -->
-                    </footer>
-                </div>
-            </main>
+            </div>
+          </main>
         </div>
     </div>
+    <div class="feedback-area" style="padding: 10px; text-align: right;">
+        <a href="feedback"><button type="button" class="btn btn-link btn-sm" >Send us your feedback!</button></a>
+    </div>
+
+     <!-- Footer -->
+
+     <footer class="text-center text-lg-start bg-light text-muted">
+            <!-- Section: Social media -->
+            <section
+                class="
+                    d-flex
+                    justify-content-center justify-content-lg-between
+                    p-4
+                    border-bottom
+                "
+            >
+                <!-- Left -->
+                <div class="me-5 d-none d-lg-block">
+                    <span>Get connected with us on social networks:</span>
+                </div>
+                <!-- Left -->
+            
+                <!-- Right -->
+                <div>
+                    <a href="" class="me-4 text-reset">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="" class="me-4 text-reset">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="" class="me-4 text-reset">
+                        <i class="fab fa-google"></i>
+                    </a>
+                    <a href="" class="me-4 text-reset">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="" class="me-4 text-reset">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </div>
+                <!-- Right -->
+            </section>
+            <!-- Section: Social media -->
+        
+            <!-- Section: Links  -->
+            <section class="">
+                <div class="container text-center text-md-start mt-5">
+                    <!-- Grid row -->
+                    <div class="row mt-3">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <!-- Content -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                <i class="fas fa-gem me-3"></i>CatatYuk!
+                            </h6>
+                            <p>
+                                Kami ingin membantu menjadikan aktifitas
+                                mencatat semua transaksi, membuat laporan
+                                keuangan, pencatatan stok barang, dan mencatat &
+                                menagih hutang piutang usaha kamu menjadi
+                                semakin mudah.
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+                    
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                Products
+                            </h6>
+                            <p>
+                                <a href="#!" class="text-reset"
+                                    >Syarat & Ketentuan</a
+                                >
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset"
+                                    >Kebijakan Privasi</a
+                                >
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">FAQ</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+                    
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                Perusahaan
+                            </h6>
+                            <p>
+                                <a href="#!" class="text-reset">Tentang Kami</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">Karir</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset"
+                                    >Blog & Cerita</a
+                                >
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">Kontak Kami</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+                    
+                        <!-- Grid column -->
+                        <div
+                            class="
+                                col-md-4 col-lg-3 col-xl-3
+                                mx-auto
+                                mb-md-0 mb-4
+                            "
+                        >
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                            <p>
+                                <i class="fas fa-home me-3"></i> Jakarta,
+                                Jakarta Selatan 12530, Indonesia
+                            </p>
+                            <p>
+                                <i class="fas fa-envelope me-3"></i>
+                                CatatYuk@gmail.com
+                            </p>
+                            <p>
+                                <i class="fas fa-phone me-3"></i> + 62 878 8652
+                                8278
+                            </p>
+                            <p>
+                                <i class="fas fa-print me-3"></i> + 01 234 567
+                                89
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!-- Grid row -->
+                </div>
+            </section>
+            <!-- Section: Links  -->
+        
+            <!-- Copyright -->
+            <div
+                class="text-center p-4"
+                style="background-color: rgba(0, 0, 0, 0.05)"
+            >
+                © 2021 Copyright:
+                <a class="text-reset fw-bold" href="https://CatatYuk.com/"
+                    >CatatYuk.com</a
+                >
+            </div>
+            <!-- Copyright -->
+        </footer>
+                
 
     <!-- JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
