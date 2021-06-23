72e0d1e5c57fc845a15cead8cdbf80095e1eeaeb
DickyArdi02 <dicky.ardi@binus.ac.id>
2021-06-24 00:05:45 +0700

    Update footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -208,7 +208,7 @@
     </div>
 
      <!-- Footer -->
-     <div class="container p-4">
+   
      <footer class="text-center text-lg-start bg-light text-muted">
             <!-- Section: Social media -->
             <section
@@ -359,7 +359,7 @@
             </div>
             <!-- Copyright -->
         </footer>
-    </div>
+ 
 
     <!-- JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
