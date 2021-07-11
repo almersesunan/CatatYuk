76da1d8aba21a17d6bec940bfcaefc344fdc8466
almersesunan <almer.sesunan@gmail.com>
2021-07-12 00:08:23 +0700

    validation error modal edit yg bikin input data ke-2 error

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,11 +25,17 @@
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
                   <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
                 </a>
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                  <li><a href="#" class="nav-link px-2 link-dark">New Book</a></li>
-                </ul>
-        
-                <div class="dropdown text-end">
+               
+                  <div class="btn-group col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                      Current Book
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Add New Book</a></li>
+                    </ul>
+                  </div>
+
+                <div class="dropdown text-end" style="align-items: flex-end">
                     <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                       <img src="https://github.com/mdo.png" alt="user" width="32" height="32" class="rounded-circle">
                     </a>
